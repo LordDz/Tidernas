69f3b8520cf572d669acb0847ab07b61bf0842bb
David <david_zetterdahl@hotmail.com>
2022-11-19 18:46:49 +0100

    Worker names and pictures

--- Assets/_Game/Scripts/Workers/WorkersForHire.cs
+++ Assets/_Game/Scripts/Workers/WorkersForHire.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-
+using UnityEngine.UI;
 
 public class WorkersForHire : MonoBehaviour
 {
@@ -13,13 +14,39 @@ public class WorkersForHire : MonoBehaviour
     [SerializeField]
     private Sprite defaultSprite;
 
+    List<WorkerInfo> listWorkers = new List<WorkerInfo>();
+
+    [SerializeField]
+    InfoText personText;
+
+    [SerializeField]
+    Image personImage;
+
+    public void AddNewWorker(WorkerInfo worker)
+    {
+        listWorkers.Add(worker);
+    }
+
+    public WorkerInfo GetWorker(int index)
+    {
+        return listWorkers[index];
+    }
+
+    public void KillWorker(int index)
+    {
+        if (listWorkers[index] != null)
+        {
+            listWorkers.RemoveAt(index);
+        }
+    }
+
     public WorkerInfo GetWorkerInfo()
     {
         int randName = Random.Range(0, ListNames.Count - 1);
-        int randSprite = Random.Range(0, ListNames.Count - 1);
+        int randSprite = Random.Range(0, ListSprites.Count - 1);
 
-        string personName = randName > ListNames.Count ? ListNames[randName] : defaultName;
-        Sprite personSprite = randSprite > ListSprites.Count ? ListSprites[randSprite] : defaultSprite;
+        string personName = randName < ListNames.Count ? ListNames[randName] : defaultName;
+        Sprite personSprite = randSprite < ListSprites.Count ? ListSprites[randSprite] : defaultSprite;
 
         if (ListNames[randName] != null)
         {
@@ -27,6 +54,8 @@ public class WorkersForHire : MonoBehaviour
         }
 
         WorkerInfo workerInfo = new(personName, personSprite);
+        personText.SetText(personName);
+        personImage.sprite = personSprite;
         return workerInfo;
     }
 }

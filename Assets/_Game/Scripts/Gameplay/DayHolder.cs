a9193abce9a81bda81de875e9cf641314fd04e38
magicandcode <magicandcode@gmail.com>
2022-11-19 13:04:25 +0100

    Fix current day condition

--- Assets/_Game/Scripts/Gameplay/DayHolder.cs
+++ Assets/_Game/Scripts/Gameplay/DayHolder.cs
@@ -55,7 +55,7 @@ public class DayHolder : MonoBehaviour
     {
         currentDay++;
 
-        if (currentDay > DAY_NEXT_WEEK)
+        if (currentDay >= DAY_NEXT_WEEK)
         {
             currentDay = 0;
             currentWeek++;

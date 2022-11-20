62d7be0f7ecc1149af7028d4aaf9fc84860cd370
David <david_zetterdahl@hotmail.com>
2022-11-20 10:49:28 +0100

    Buttons for hiring and finding new workers wip icons

--- Assets/_Game/Scripts/Menu/BtnChoice.cs
+++ Assets/_Game/Scripts/Menu/BtnChoice.cs
@@ -12,22 +12,22 @@ public class BtnChoice : MonoBehaviour
         resourceHolder = FindObjectOfType<ResourceHolder>();
     }
 
-    public void ChoiceOne()
+    public void ChoiceAddWorkerToFishing()
     {
         resourceHolder.AddActiveWorker(JobChoice.fishing);
     }
 
-    public void ChoiceTwo()
+    public void ChoiceAddWorkerToFactory()
     {
         resourceHolder.AddActiveWorker(JobChoice.factory);
     }
 
-    public void ChoiceThree()
+    public void ChoiceWorkerHire()
     {
         resourceHolder.HireWorker();
     }
 
-    public void ChoiceFindNewWorker()
+    public void ChoiceWorkerFindNew()
     {
         Debug.Log("FIND NEW WORKER CLICKED ON!?");
         resourceHolder.FindNewWorker();

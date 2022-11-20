9f4847d3998b7c013244cab36113767804a0aec9
magicandcode <magicandcode@gmail.com>
2022-11-20 10:44:40 +0100

    Implement simple high score display

--- Assets/_Game/Scripts/Menu/HighScoreHandler.cs
+++ Assets/_Game/Scripts/Menu/HighScoreHandler.cs
@@ -9,11 +9,21 @@ public class HighScoreHandler : MonoBehaviour
     DayHolder dayHolder;
     Player player;
 
+    [SerializeField]
+    InfoText scoreValueText, scoreMessage, title;
+
+    [SerializeField]
+    TextMeshProUGUI fameText;
+
     void Start()
     {
         resourceHolder = FindObjectOfType<ResourceHolder>();
         dayHolder = FindObjectOfType<DayHolder>();
         player = FindObjectOfType<Player>();
+        title.gameObject.SetActive(false);
+        fameText.gameObject.SetActive(false);
+        scoreValueText.gameObject.SetActive(false);
+        scoreMessage.gameObject.SetActive(false);
     }
 
     public void ApplyScore()
@@ -21,10 +31,13 @@ public class HighScoreHandler : MonoBehaviour
         string key = GetKey();
         int score = GetScore();
         int previousScore = PlayerPrefs.GetInt(key, 0);
+        bool isNew = false;
         if (score > previousScore)
         {
             PlayerPrefs.SetInt(key, score);
+            isNew = true;
         }
+        ShowHighScore(key, score, isNew);
     }
 
     public int GetScore()
@@ -36,5 +49,24 @@ public class HighScoreHandler : MonoBehaviour
     {
         return player.name.ToLower().Replace(" ", "-");
         // name = String.Concat(name.Where(c => !Char.IsWhiteSpace(c)));
+    }
+
+    public void ShowHighScore(string key, int score, bool isNew)
+    {
+        string message;
+        if (isNew)
+        {
+            message = "You did it! " + score.ToString() + " is your new personal best, well done.";
+        }
+        else
+        {
+            message = "Good job, but not as good as your best score. You can do better!";
+        }
+        scoreValueText.SetText(score.ToString() + " fame");
+        scoreMessage.SetText(message);
+        title.gameObject.SetActive(true);
+        fameText.gameObject.SetActive(true);
+        scoreValueText.gameObject.SetActive(true);
+        scoreMessage.gameObject.SetActive(true);
     }
 }

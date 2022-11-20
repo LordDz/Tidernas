a128e21648f3035295a99ac329de8a24377129b2
David <david_zetterdahl@hotmail.com>
2022-11-20 16:19:17 +0100

    aaaaaaaa

--- Assets/_Game/Scripts/Gameplay/JobWorkForce.cs
+++ Assets/_Game/Scripts/Gameplay/JobWorkForce.cs
@@ -56,6 +56,8 @@ public class JobWorkForce : MonoBehaviour
 
         highest -= skill;
         highest += tired;
+        Debug.Log("MaxChance - highest: " + highest);
+
         return highest;
     }
 
